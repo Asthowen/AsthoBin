ed15dfe8d4897cf469bb9b82b2e4598654e08afd
Asthowen <raphael.pilinski@protonmail.com>
2024-10-26 01:32:26 +0200

    chore: improve code highlighting display

--- frontend/ts/code.ts
+++ frontend/ts/code.ts
@@ -1,32 +1,24 @@
 import hljs from "highlight.js";
 import { languageExtensions } from "./languages_extension";
 
-const codeArea = document.getElementById("codeArea")! as HTMLTextAreaElement;
-
-hljs.highlightElement(codeArea);
-
-const detectedLanguage =
-  codeArea.className
-    .split(" ")
-    .find((cls) => cls.startsWith("language-"))
-    ?.substring(9) ?? "txt";
-
-setTimeout(
-  () => document.getElementById("codeAreaPre")!.classList.remove("hidden"),
-  100,
-);
+const codeContainer = document.getElementById("codeContainer")!;
+const { value: highlightedCode, language: detectedLanguage } =
+  hljs.highlightAuto(codeContainer.textContent || "");
+codeContainer.innerHTML = highlightedCode;
+codeContainer.classList.add("hljs");
+codeContainer.classList.remove("hidden");
 
 document.getElementById("generateFile")!.onclick = () => {
-  if (codeArea.textContent === null) return;
+  if (codeContainer.innerText === null) return;
 
-  const data = new Blob([codeArea.textContent], {
+  const data = new Blob([codeContainer.innerText], {
     type: "text/plain",
   });
   const objectURL = window.URL.createObjectURL(data);
-  const a = document.createElement("a");
-  a.style.display = "none";
-  a.href = objectURL;
-  a.download = `${window.location.pathname.split("/")[1]}.${languageExtensions[detectedLanguage] ?? "txt"}`;
-  a.click();
+  const downloadLink = document.createElement("a");
+  downloadLink.style.display = "none";
+  downloadLink.href = objectURL;
+  downloadLink.download = `${window.location.pathname.split("/")[1]}.${languageExtensions[detectedLanguage ?? ""] ?? "txt"}`;
+  downloadLink.click();
   window.URL.revokeObjectURL(objectURL);
 };
